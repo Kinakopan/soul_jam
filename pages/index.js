83878bb0909cd128d371ea5c0d363777cc3c04c8
Kinakopan <mtakagi@my.bcit.ca>
2022-11-21 19:55:40 -0800

    made all the parts and added images

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import styled from 'styled-components';
 import styles from '../styles/Home.module.css'
 import SideBar from '../components/sidebar/sidebar';
+import Follower from '../components/follower/follower';
 
+const Wrapper = styled.div`
+  display:flex;
+  flex-direction: row;
+  width: 100%;
+  // background-color: #FFFFFF;
+  height:100vh;
+  align-items: space-between;
+  justify-content: space-between;
+  // padding-top:3%;
+  // box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+`
 
 export default function Home() {
   return (
-    <div>
+    <Wrapper>
       <SideBar></SideBar>
-    </div>
+
+
+      <Follower></Follower>
+    </Wrapper>
   )
 }
